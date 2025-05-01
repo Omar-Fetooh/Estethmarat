fb2892151341e4fa4dca9298554f6d092896c945
kareemtarekK <engkareemtarek555@gmail.com>
2025-05-01 20:51:49 +0300

    Search & Filter

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(express.json({ limit: '10kb' }));
 // parse cookie
 app.use(cookieParser());
 app.use('/api/v1/search', router.searchRouter);
-app.use('/api/v1/filter', router.searchRouter);
+// app.use('/api/v1/filter', router.searchRouter);
 
 app.use('/api/v1/auth', router.authRouter);
 app.use('/api/v1/organizations', router.organizationRouter);
@@ -51,7 +51,7 @@ app.use('/api/v1/posts', router.postRouter);
 app.use('/api/v1/reviews', router.reviewRouter);
 app.use('/api/v1/donations', router.donationRouter);
 app.use('/api/v1/deal', router.dealRouter);
-app.use('/api/v1/comments',router.commentRouter);
+app.use('/api/v1/comments', router.commentRouter);
 
 app.use('/api/v1/consultations', router.consultationRouter);
 app.use('/api/v1/questions', router.questionRouter);
--- src/modules/search.js
+++ src/modules/search.js
@@ -10,7 +10,6 @@ export const searchRouter = Router();
 
 const getSearchResult = errorHandler(async (req, res, next) => {
   const { name } = req.query;
-  console.log(name);
   const investors = await Investor.find({
     fullEnglishName: { $regex: name, $options: 'i' },
   });
@@ -23,36 +22,24 @@ const getSearchResult = errorHandler(async (req, res, next) => {
   const charityOrganizations = await CharityOrganization.find({
     name: { $regex: name, $options: 'i' },
   });
-  const userResults = [
+  let userResults = [
     ...investors,
     ...companies,
     ...supportOrganizations,
     ...charityOrganizations,
   ];
-  await SearchResult.deleteMany({});
-  await SearchResult.create({
-    query: name,
-    result: JSON.stringify(userResults),
-  });
-  await res.status(200).json({
-    status: 'success',
-    length: userResults.length,
-    userResults,
-  });
-});
-const filterBody = errorHandler(async (req, res, next) => {
-  const searchedResult = await SearchResult.find();
-  let arrResult = JSON.parse(searchedResult[0].result);
+
   const arrOfPosts = await Post.find({
     title: {
-      $regex: searchedResult[0].query,
+      $regex: name,
       $options: 'i',
     },
   });
+
   if (req.body.accountType === 'منشور') {
-    arrResult = [...arrOfPosts];
+    userResults = [...arrOfPosts];
   } else {
-    arrResult = arrResult.filter((obj) => {
+    userResults = userResults.filter((obj) => {
       if (req.body.accountType === 'مستثمر' && obj.role === 'investor') {
         return obj;
       } else if (req.body.accountType === 'شركة' && obj.role === 'company') {
@@ -72,9 +59,14 @@ const filterBody = errorHandler(async (req, res, next) => {
       }
     });
   }
+  await SearchResult.deleteMany({});
+  await SearchResult.create({
+    query: name,
+    result: JSON.stringify(userResults),
+  });
 
-  if (arrResult[0]?.role === 'supportOrganization') {
-    arrResult = arrResult.filter((obj) => {
+  if (userResults[0]?.role === 'supportOrganization') {
+    userResults = userResults.filter((obj) => {
       if (
         (req.body.organizationType === undefined ||
           obj.organizationType === req.body.organizationType) &&
@@ -87,8 +79,8 @@ const filterBody = errorHandler(async (req, res, next) => {
         return obj;
       }
     });
-  } else if (arrResult[0]?.role === 'charityOrganization') {
-    arrResult = arrResult.filter((obj) => {
+  } else if (userResults[0]?.role === 'charityOrganization') {
+    userResults = userResults.filter((obj) => {
       if (
         (req.body.charityCountry === undefined ||
           obj.country === req.body.charityCountry) &&
@@ -101,9 +93,8 @@ const filterBody = errorHandler(async (req, res, next) => {
         return obj;
       }
     });
-  } else if (arrResult[0]?.role === 'company') {
-    arrResult = arrResult.filter((obj) => {
-      console.log(obj);
+  } else if (userResults[0]?.role === 'company') {
+    userResults = userResults.filter((obj) => {
       if (
         (req.body.businessField === undefined ||
           JSON.stringify(obj.companyField) ===
@@ -124,10 +115,97 @@ const filterBody = errorHandler(async (req, res, next) => {
   }
   res.status(200).json({
     status: 'success',
-    length: arrResult.length,
-    arrResult,
+    length: userResults.length,
+    userResults,
   });
 });
+// const filterBody = errorHandler(async (req, res, next) => {
+//   const searchedResult = await SearchResult.find();
+//   let arrResult = JSON.parse(searchedResult[0].result);
+//   const arrOfPosts = await Post.find({
+//     title: {
+//       $regex: searchedResult[0].query,
+//       $options: 'i',
+//     },
+//   });
+//   if (req.body.accountType === 'منشور') {
+//     arrResult = [...arrOfPosts];
+//   } else {
+//     arrResult = arrResult.filter((obj) => {
+//       if (req.body.accountType === 'مستثمر' && obj.role === 'investor') {
+//         return obj;
+//       } else if (req.body.accountType === 'شركة' && obj.role === 'company') {
+//         return obj;
+//       } else if (
+//         req.body.accountType === 'منظمة خيرية' &&
+//         obj.role === 'charityOrganization'
+//       ) {
+//         return obj;
+//       } else if (
+//         req.body.accountType === 'منظمة داعمة' &&
+//         obj.role === 'supportOrganization'
+//       ) {
+//         return obj;
+//       } else if (Object.keys(req.body).length === 0) {
+//         return obj;
+//       }
+//     });
+//   }
 
-searchRouter.get('/', getSearchResult);
-searchRouter.post('/', filterBody);
+//   if (arrResult[0]?.role === 'supportOrganization') {
+//     arrResult = arrResult.filter((obj) => {
+//       if (
+//         (req.body.organizationType === undefined ||
+//           obj.organizationType === req.body.organizationType) &&
+//         (req.body.supportCountry === undefined ||
+//           obj.country === req.body.supportCountry) &&
+//         (req.body.supportedProjectFields === undefined ||
+//           JSON.stringify(obj.supportedProjectFields) ===
+//             JSON.stringify(req.body.supportedProjectFields))
+//       ) {
+//         return obj;
+//       }
+//     });
+//   } else if (arrResult[0]?.role === 'charityOrganization') {
+//     arrResult = arrResult.filter((obj) => {
+//       if (
+//         (req.body.charityCountry === undefined ||
+//           obj.country === req.body.charityCountry) &&
+//         (req.body.charityOrganizationType === undefined ||
+//           obj.organizationType === req.body.charityOrganizationType) &&
+//         (req.body.charityFieldsSupported === undefined ||
+//           JSON.stringify(obj.projectTypes) ===
+//             JSON.stringify(req.body.charityFieldsSupported))
+//       ) {
+//         return obj;
+//       }
+//     });
+//   } else if (arrResult[0]?.role === 'company') {
+//     arrResult = arrResult.filter((obj) => {
+//       if (
+//         (req.body.businessField === undefined ||
+//           JSON.stringify(obj.companyField) ===
+//             JSON.stringify(req.body.businessField)) &&
+//         (req.body.country === undefined || obj.country === req.body.country) &&
+//         (req.body.stage === undefined || obj.state === req.body.stage) &&
+//         (req.body.wantedServices === undefined ||
+//           JSON.stringify(Object.keys(obj.requiredServices)) ===
+//             JSON.stringify(req.body.wantedServices)) &&
+//         (req.body.rangeTo === undefined ||
+//           req.body.rangeFrom === undefined ||
+//           (obj.investmentAmount >= Number(req.body.rangeFrom) &&
+//             obj.investmentAmount <= Number(req.body.rangeTo)))
+//       ) {
+//         return obj;
+//       }
+//     });
+//   }
+//   res.status(200).json({
+//     status: 'success',
+//     length: arrResult.length,
+//     arrResult,
+//   });
+// });
+
+searchRouter.post('/', getSearchResult);
+// searchRouter.post('/', filterBody);
