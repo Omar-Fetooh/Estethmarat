0ab0f4e5495f81829ba97a702467fe2b6ca03637
kareemtarekK <engkareemtarek555@gmail.com>
2025-06-13 01:44:33 +0300

    fix bug in recomendations

--- src/modules/suggestionCompanies/suggestionCompanies.controller.js
+++ src/modules/suggestionCompanies/suggestionCompanies.controller.js
@@ -13,7 +13,7 @@ export const getRecomendations = errorHandler(async (req, res, next) => {
     (await Investor.findById(userId).select('role -_id')) ||
     (await Company.findById(userId).select('role -_id')) ||
     (await CharityOrganization.findById(userId).select('role -_id')) ||
-    (await CharityOrganization.findById(userId).select('role -_id'));
+    (await supportOrganization.findById(userId).select('role -_id'));
   if (role === 'investor') {
     const suggestedCompanies = await SuggestedCom.find()
       .populate({
