b65d401f2eed9d304d21b5cc63fdb4eb5472db2f
kareemtarekK <engkareemtarek555@gmail.com>
2025-06-28 02:01:08 +0300

    add score in response

--- src/modules/statisticsInvestors/statisticsInvestor.controller.js
+++ src/modules/statisticsInvestors/statisticsInvestor.controller.js
@@ -7,10 +7,12 @@ export const getTopThreeInvestors = errorHandler(async (req, res, next) => {
     .sort({ score: -1 })
     .limit(3);
   const topInvestors = await Promise.all(
-    topThreeInvestors.map(
-      async (investor) =>
-        await Investor.findOne({ recommendation_id: investor.investor_id })
-    )
+    topThreeInvestors.map(async (investor) => {
+      const investor_result = await Investor.findOne({
+        recommendation_id: investor.investor_id,
+      });
+      return { investor_result, score: investor.score };
+    })
   );
 
   res.status(200).json({
